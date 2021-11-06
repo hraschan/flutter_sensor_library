41edb3e340a62f6ddf7d6ada2ddf43c464d9df8a
hraschan <office@mhraschan.com>
2021-11-06 15:59:49 +0100

    add Barometer class

--- lib/sensor_libary.dart
+++ lib/sensor_libary.dart
@@ -1,9 +1,15 @@
 library sensor_libary;
 
+import 'dart:ffi';
+
 /// A Calculator.
 class Calculator {
   /// Returns [value] plus 1.
   int addOne(int value) => value + 1;
   int removeOne(int value) => value - 1;
   int resetValue() => 0;
+}
+
+class Barometer {
+  double getPressure() => 12312.76;
 }
