50bab0b3357f17ededacf9922aa87b1bb5c5dd7e
hraschan <office@mhraschan.com>
2021-11-25 13:44:39 +0100

    Test barometer sensor

--- lib/models/raw_sensors/barometer.dart
+++ lib/models/raw_sensors/barometer.dart
@@ -1,10 +1,21 @@
 import 'sensor.dart';
+import 'package:barometer_plugin/barometer_plugin.dart';
 
 class Barometer extends Sensor {
   @override
-  double getRaw() {
-    // TODO: implement getRaw
-    throw UnimplementedError();
+  Future<double> getRaw() async {
+    var list = await BarometerPlugin.getBarometerData();
+    return list[list.length - 1];
   }
-  
+
+  @override
+  startTracking(int inMillis) async {
+    var seconds = (inMillis / 1000).round();
+    return await BarometerPlugin.startBarometerListener(seconds);
+  }
+
+  @override
+  stopTracking() async {
+    return await BarometerPlugin.stopBarometerListener();
+  }
 }
--- lib/models/value_interpret/position.dart
+++ lib/models/value_interpret/position.dart
@@ -1,17 +1,35 @@
 import 'package:sensor_library/models/enums/length_unit.dart';
+import 'package:sensor_library/models/raw_sensors/barometer.dart';
 import 'package:sensor_library/models/return_types/direction.dart';
+import 'package:sensor_library/models/time_series.dart';
 import 'package:sensor_library/models/value_interpret/sensor_type.dart';
 
-class Position extends SensorType {
+class Position extends TimeSeries {
+  late Barometer barometer;
+
+  Position() {
+    barometer = Barometer();
+  }
+
+  @override
+  startTracking(int inMillis) {
+    barometer.startTracking(inMillis);
+  }
+
   Direction getCurrentDirection() {
-    return Direction(looksNorth: true, looksEast: false, looksSouth: false, looksWest: false);
+    return Direction(
+        looksNorth: true,
+        looksEast: false,
+        looksSouth: false,
+        looksWest: false);
   }
 
   double getCurrentHeading() {
     return 345;
   }
 
-  double getAltitute(LengthUnit unit) {
-    return 329.25;
+  Future<double> getAltitute(LengthUnit unit) async {
+    var rawData = await barometer.getRaw();
+    return rawData;
   }
 }
