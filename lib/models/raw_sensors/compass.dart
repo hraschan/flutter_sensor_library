f525899efc4307a75469d8466e33cd3edbb6ae52
it201527 <it201527@fhstp.ac.at>
2021-12-13 21:42:26 +0100

    implements raw compass

--- lib/models/raw_sensors/compass.dart
+++ lib/models/raw_sensors/compass.dart
@@ -1,11 +1,22 @@
+import 'package:flutter_compass/flutter_compass.dart';
 
 import 'sensor.dart';
 
 class Compass extends Sensor {
+  int inMillis;
+  Compass({required this.inMillis});
+
   @override
-  CompassEvent getRaw() {
-    // TODO: implement getRaw
-    throw UnimplementedError();
+  Stream<CompassEvent> getRaw() {
+    var timestampAtLastCall = DateTime.now().millisecondsSinceEpoch;
+    return FlutterCompass.events!.where((event) {
+      if (DateTime.now().millisecondsSinceEpoch - timestampAtLastCall >
+          inMillis) {
+        timestampAtLastCall = DateTime.now().millisecondsSinceEpoch;
+        return true;
+      }
+      return false;
+    });
   }
   
 }
