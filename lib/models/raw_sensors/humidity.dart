81e9a272288a9db4095ce2588a9111c6ae906af7
it201527 <it201527@fhstp.ac.at>
2022-02-27 17:01:34 +0100

    Adds Temperature and Humidity Sensor

---
+++ lib/models/raw_sensors/humidity.dart
@@ -0,0 +1,32 @@
+import 'package:environment_sensors/environment_sensors.dart';
+import 'package:flutter/services.dart';
+import 'package:sensor_library/models/raw_sensors/sensor.dart';
+
+class Humidity extends Sensor {
+
+  int inMillis;
+  final _environmentSensors = EnvironmentSensors();
+
+  Humidity({required this.inMillis}) {
+    checkIfAvailable();
+  }
+
+  checkIfAvailable() async {
+    if(!(await _environmentSensors.getSensorAvailable(SensorType.Humidity))){
+      print("Humidity sensor not available.");
+      throw PlatformException(code: "SENSOR_NOT_AVAILABLE", message: "Humidity sensor not available.");
+    }
+  }
+
+  @override
+  Stream<double> getRaw() {
+    var timestampAtLastCall = DateTime.now().millisecondsSinceEpoch;
+    return _environmentSensors.humidity.where((event) {
+      if (DateTime.now().millisecondsSinceEpoch - timestampAtLastCall > inMillis) {
+        timestampAtLastCall = DateTime.now().millisecondsSinceEpoch;
+        return true;
+      }
+      return false;
+    });
+  }
+}
