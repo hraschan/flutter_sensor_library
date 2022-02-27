8f320e825a6ca956fd6e3235463980361e5dc6cb
it201527 <it201527@fhstp.ac.at>
2022-02-27 23:39:22 +0100

    Adds proximity sensor

--- lib/models/raw_sensors/proximity.dart
+++ lib/models/raw_sensors/proximity.dart
@@ -1,18 +1,30 @@
 import 'package:sensor_library/models/raw_sensors/sensor.dart';
+import 'package:proximity_sensor/proximity_sensor.dart';
 
 import '../../sensor_library.dart';
 
 class Proximity extends Sensor {
 
-  Proximity() {
+  int inMillis;
+
+  Proximity({required this.inMillis}) {
     Library.checkIfOnWebProject();
-    throw UnimplementedError("Proximity sensor not available.");
   }
 
   @override
-  int getRaw() {
-    // TODO: implement getRaw
-    throw UnimplementedError("Proximity sensor not available.");
+  Stream<int> getRaw() {
+    var timestampAtLastCall = DateTime.now().millisecondsSinceEpoch;
+    return ProximitySensor.events.where((event) {
+      if (DateTime.now().millisecondsSinceEpoch - timestampAtLastCall > inMillis) {
+        timestampAtLastCall = DateTime.now().millisecondsSinceEpoch;
+        return true;
+      }
+      return false;
+    });
+  }
+
+  Stream<int> getRawWithoutTimelimit() {
+    return ProximitySensor.events;
   }
   
 }
